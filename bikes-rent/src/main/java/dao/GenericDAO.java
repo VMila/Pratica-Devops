970545b61873786bbf1da32b3721b5e5dd374b57
VMila <vi-milanez@hotmail.com>
2025-05-21 21:55:26 -0300

    trabalho devops

--- bikes-rent/src/main/java/dao/GenericDAO.java
+++ bikes-rent/src/main/java/dao/GenericDAO.java
@@ -18,16 +18,26 @@ public class GenericDAO {
 
     protected Connection getConnection() throws SQLException {
 
-        String url = "jdbc:mysql://localhost:3306/bikeRentSystem?serverTimezone=America/Sao_Paulo";
-        Dotenv dotenv = Dotenv.load();
-
-        String dbpass= dotenv.get("DB_PASSWORD");
+        Dotenv dotenv = Dotenv.configure()
+            .filename(".env")
+            .directory("/usr/local/tomcat")
+            .ignoreIfMalformed()
+            .ignoreIfMissing()
+            .systemProperties()
+            .load();
+        String host = dotenv.get("DB_HOST");
+        String port = dotenv.get("DB_PORT");
+        String db = dotenv.get("DB_NAME");
         String dbUser = dotenv.get("DB_USER");
-        
-        // return DriverManager.getConnection(url, "root", "root");
-        // configurar usuario e senha conforme o banco
+        String dbpass = dotenv.get("DB_PASSWORD");
 
-     
+        String url = "jdbc:mysql://" + host + ":" + port + "/" + db + 
+             "?useSSL=false" +
+             "&serverTimezone=America/Sao_Paulo" +
+             "&allowPublicKeyRetrieval=true" +
+             "&connectionTimeOut=30000" +
+             "&socketTimeOut=30000";
+        
         return DriverManager.getConnection(url, dbUser, dbpass);
     }
 }
